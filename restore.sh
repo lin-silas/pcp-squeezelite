ef8b05bf5e2be8fabf08c4528703f1c59eab87cc
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-14 12:06:12 +0800

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cd /mnt/sda2/tce
+wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-pcm
+wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-dsd
+wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-custom
+
