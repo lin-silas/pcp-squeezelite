057e6b0a6d1cad23f6eabacc38a0d977549d3e61
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-14 12:07:10 +0800

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -5,3 +5,6 @@ wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelit
 wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-dsd
 wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-custom
 
+ln -sf /mnt/sda2/tce/squeezelite-pcm /mnt/sda2/tce/squeezelite
+sudo /usr/local/etc/init.d/squeezelite restart
+sudo taskset -p 0x00000008 $(pgrep squeezelite*)
