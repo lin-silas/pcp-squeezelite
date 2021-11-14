7c0e9f653c06126b4180f407694d2f7818e36194
lin-silas <93635072+lin-silas@users.noreply.github.com>
2021-11-14 18:55:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-cd ~
-wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/change.sh
-sudo chmod +x change.sh
-
 cd /mnt/sda2/tce
 wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-anker
 wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-enerpad
 wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/squeezelite-mcdodo
-
+wget https://raw.githubusercontent.com/lin-silas/pcp-squeezelite/main/change.sh
+sudo chmod +x change.sh
+sudo ln -s /mnt/sda2/tce/change.sh /usr/local/bin
+sudo echo "/usr/local/bin/change”.sh >> /opt/.filetool.lst
 pcp bu
